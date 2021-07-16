c3b6b659da7b32054baf13f65f1ec7427ebd1df0
Lunarixus <Nathan@lunarixus.dev>
2021-07-16 15:41:02 +0530

    veux: Remove packages at build time
    Co-authored-by: ugly-kid-af <dwarmachine24@gmail.com>
    Co-authored-by: naveenjohnsonv <14140949+naveenjohnsonv@users.noreply.github.com>
    Co-authored-by: Arturo <rtsanonynousperson@gmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -464,6 +464,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePkgs
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.6.vendor \
---
+++ remove_packages/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := RemovePkgs
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_OVERRIDES_PACKAGES := \
+          arcore \
+          AmbientSensePrebuilt \
+          AppDirectedSMSService \
+          ConnMO \
+          CalendarGooglePrebuilt \
+          Drive \
+          DCMO \
+          DevicePolicyPrebuilt \
+          DMService \
+          GCS \
+          GoogleCamera \
+          MaestroPrebuilt \
+          Maps \
+          MicropaperPrebuilt \
+          MyVerizonServices \
+          OBDM_Permissions \
+          obdm_stub \
+          PrebuiltGmail \
+          PixelWallpapers2021 \
+          PixelWallpapers2020 \
+          RecorderPrebuilt \
+          SafetyHubPrebuilt \
+          SCONE \
+          ScribePrebuilt \
+          Showcase \
+          Snap \
+          SoundAmplifierPrebuilt \
+          SprintDM \
+          SprintHM \
+          TurboPrebuilt \
+          Tycho \
+          TipsPrebuilt \
+          talkback \
+          USCCDM \
+          VZWAPNLib \
+          VzwOmaTrigger \
+          WallpapersBReel2020 \
+          YouTube \
+          YouTubeMusicPrebuilt 
+
+LOCAL_UNINSTALLABLE_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SRC_FILES := /dev/null
+include $(BUILD_PREBUILT)
