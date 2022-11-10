SuperiorOS-Devices/device-_xiaomi_veux

veux: Merge common tree to veux
- Ref:
  https://github.com/xiaomi-sm6375-devs/android_device_xiaomi_sm6375-common

Signed-off-by: kleidione <kleidione@gmail.com>

libinit/include/libinit_variant.h
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIBINIT_VARIANT_H
+#define LIBINIT_VARIANT_H
+
+#include <string>
+#include <vector>
+
+typedef struct variant_info {
+    std::string model_value;
+    std::string sku_value;
+
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string name;
+    std::string build_fingerprint;
+
+    bool nfc;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+
+void set_variant_props(const variant_info_t variant);
+
+#endif // LIBINIT_VARIANT_H

libinit/libinit_variant.cpp
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <libinit_utils.h>
+
+#include <libinit_variant.h>
+
+using android::base::GetProperty;
+
+#define MODEL_PROP "ro.boot.board_id"
+#define SKU_PROP "ro.boot.product.hardware.sku"
+
+void search_variant(const std::vector<variant_info_t> variants) {
+    std::string model_value = GetProperty(MODEL_PROP, "");
+    std::string sku_value = GetProperty(SKU_PROP, "");
+
+    for (const auto& variant : variants) {
+        if ((variant.model_value == "" || variant.model_value == model_value) &&
+            (variant.sku_value == "" || variant.sku_value == sku_value)) {
+            set_variant_props(variant);
+            break;
+        }
+    }
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+    set_ro_build_prop("name", variant.name, true);
+
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        set_ro_build_prop("fingerprint", variant.build_fingerprint);
+        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
+
+        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+    }
+
+    if (variant.nfc)
+        property_override(SKU_PROP, "nfc");
+}

Evolution-XYZ-Devices/device_xiaomi_veux

libinit/include/libinit_variant.h
@@ -1,0 +1,31 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIBINIT_VARIANT_H
+#define LIBINIT_VARIANT_H
+
+#include <string>
+#include <vector>
+
+typedef struct variant_info {
+    std::string hwc_value;
+    std::string model_value;
+
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string name;
+    std::string build_fingerprint;
+
+    bool nfc;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+
+void set_variant_props(const variant_info_t variant);
+
+#endif // LIBINIT_VARIANT_H

libinit/libinit_variant.cpp
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <libinit_utils.h>
+
+#include <libinit_variant.h>
+
+using android::base::GetProperty;
+
+
+#define HWC_PROP "ro.boot.hwc"
+#define MODEL_PROP "ro.boot.board_id"
+#define SKU_PROP "ro.boot.product.hardware.sku"
+
+void search_variant(const std::vector<variant_info_t> variants) {
+    std::string hwc_value = GetProperty(HWC_PROP, "");
+    std::string model_value = GetProperty(MODEL_PROP, "");
+
+    for (const auto& variant : variants) {
+        if ((variant.hwc_value == "" || variant.hwc_value == hwc_value) &&
+            (variant.model_value == "" || variant.model_value == model_value)) {
+            set_variant_props(variant);
+            break;
+        }
+    }
+}
+
+void set_variant_props(const variant_info_t variant) {
+    set_ro_build_prop("brand", variant.brand, true);
+    set_ro_build_prop("device", variant.device, true);
+    set_ro_build_prop("marketname", variant.marketname, true);
+    set_ro_build_prop("model", variant.model, true);
+    set_ro_build_prop("name", variant.name, true);
+
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        set_ro_build_prop("fingerprint", variant.build_fingerprint);
+        property_override("ro.bootimage.build.fingerprint", variant.build_fingerprint);
+
+        property_override("ro.build.description", fingerprint_to_description(variant.build_fingerprint));
+    }
+
+    if (variant.nfc)
+        property_override(SKU_PROP, "nfc");
+}