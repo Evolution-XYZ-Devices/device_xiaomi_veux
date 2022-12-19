d7b62ade612e0511949ad6024f848816abdadb65
JaswalAshish <jaswalashish48@gmail.com>
2022-12-19 14:06:35 +0530

    veux: Add vendorsetup.sh
    Signed-off-by: JaswalAshish <jaswalashish48@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,21 @@
+echo 'Starting to clone stuffs needed for your device'
+# Stuffs to rm -rf
+rm -rf hardware/qcom-caf/sm8350/display
+
+echo 'Cloning Hals [1/4]'
+# Hals
+git clone https://github.com/JaswalAshish/hardware_qcom-caf_sm8350_display.git hardware/qcom-caf/sm8350/display
+
+echo 'Cloning Vendor tree [2/4]'
+# Vendor Tree
+git clone https://github.com/JaswalAshish/vendor_xiaomi_veux.git vendor/xiaomi/veux
+
+echo 'Cloning Kernel tree [3/4]'
+# Kernel Tree
+git clone --depth=1 https://github.com/JaswalAshish/kernel_xiaomi_sm8350.git kernel/xiaomi/sm6375
+
+echo 'Cloning Prebuilt Kernel tree [4/4]'
+# Prebuilt Kernel Tree
+git clone https://github.com/JaswalAshish/device_xiaomi_veux-kernel.git device/xiaomi/veux-kernel
+
+echo 'Completed, proceeding to lunch'
