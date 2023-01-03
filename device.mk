5bc0135c42088c7ec189601b9f5f7268ec06d516
JaswalAshish <jaswalashish48@gmail.com>
2023-01-03 21:33:28 +0530

    veux: Drop XiaomiParts

--- device.mk
+++ device.mk
@@ -138,6 +138,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
+# DeviceSettings
+#PRODUCT_PACKAGES += \
+#    XiaomiParts
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
