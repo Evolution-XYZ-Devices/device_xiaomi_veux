851d72034be616e1dc1af82e9daec94880c859b4
kdrag0n <danny@kdrag0n.dev>
2023-03-29 11:21:16 +0800

    veux: rro_overlay: Compact cached app heaps in the background
    * Background ART heap compaction for cached apps has been recommended for Android Go devices for a while, and Google has enabled it for the Pixel 5 on RQ1A.201205.010. This feature should help fit more cached apps in memory
    * edit: use RRO
    
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -327,6 +327,7 @@ PRODUCT_PACKAGES += \
     SettingsProviderOverlayS88106BA1 \
     SettingsProviderOverlayS88107BA1 \
     SystemUIOverlayVeux \
+    SimpleDeviceConfigOverlayVeux \
     TelephonyOverlayVeux \
     WifiOverlayVeux \
     WifiOverlayS88006AA1 \
