a6c4acda7958f7252cdd06b743906bf0031dc7a7
nnippon99 <adamayyad1999@hotmail.com>
2023-04-18 17:50:59 +0300

    veux: update devices FP to the latest and import other changes * Blobs extracted from: Redmi/veux_global/veux:12/SKQ1.211006.001/V13.0.7.0.SKCMIXM:user/release-keys
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -201,7 +201,7 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2022-11-01
+VENDOR_SECURITY_PATCH := 2023-03-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
