2621aecac59e6df81eda18aa3b081a60d4223815
Pranav Vashi <neobuddy89@gmail.com>
2023-05-16 16:18:03 +0300

    veux: rootdir: Tune schedutil parameters
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- rootdir/bin/init.kernel.post_boot-blair.sh
+++ rootdir/bin/init.kernel.post_boot-blair.sh
@@ -155,15 +155,15 @@ echo 0 > /proc/sys/kernel/sched_boost
 
 # configure governor settings for silver cluster
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
+echo 10000 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/down_rate_limit_us
+echo 500 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/up_rate_limit_us
 echo 1113600 > /sys/devices/system/cpu/cpufreq/policy0/schedutil/hispeed_freq
 echo 576000 > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
 
 # configure governor settings for gold cluster
 echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
-echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
-echo 0 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
+echo 15000 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/down_rate_limit_us
+echo 500 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/up_rate_limit_us
 echo 1228800 > /sys/devices/system/cpu/cpufreq/policy6/schedutil/hispeed_freq
 echo 691200 > /sys/devices/system/cpu/cpufreq/policy6/scaling_min_freq
 
