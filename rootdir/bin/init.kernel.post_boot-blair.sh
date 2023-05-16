8897a1d69783960fed42c65c604397a62b1f2718
Nippon <126399437+nnippon99@users.noreply.github.com>
2023-05-16 17:38:41 +0300

    veux: Fix build error * Duplicated stuff
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- rootdir/bin/init.kernel.post_boot-blair.sh
+++ rootdir/bin/init.kernel.post_boot-blair.sh
@@ -140,6 +140,7 @@ echo 0-1 > /dev/cpuset/background/cpus
 echo 0-3 > /dev/cpuset/restricted/cpus
 echo 0-3 > /dev/cpuset/system-background/cpus
 echo 0-6 > /dev/cpuset/foreground/cpus
+echo 0-7     > /dev/cpuset/top-app/cpus
 
 # Turn off scheduler boost at the end
 echo 0 > /proc/sys/kernel/sched_boost
