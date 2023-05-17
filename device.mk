22ce98d022aacc1fcab082af4c2b3eb2504e15a7
nnippon99 <adamayyad1999@hotmail.com>
2023-05-17 21:23:43 +0300

    veux: inherit dalvik-heap vm configs
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -19,6 +19,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
+
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
