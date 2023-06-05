4d75d79cbd5a920d8f7668b3015c52c9b54e1011
nnippon99 <adamayyad1999@hotmail.com>
2023-06-05 04:39:38 +0300

    veux: switch to prebuilt libdisplayconfig.qti.so
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -159,7 +159,6 @@ PRODUCT_PACKAGES += \
     memtrack.default
 
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
