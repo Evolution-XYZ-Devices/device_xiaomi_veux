4f0990cbe3f047341d308382dc2501bf6eb03601
nnippon99 <adamayyad1999@hotmail.com>
2023-06-14 13:33:06 +0300

    veux: device.mk: build libOmxVidcCommon package
    Signed-off-by: nnippon99 <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -332,6 +332,7 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
+    libOmxVidcCommon \
     libstagefrighthw \
     libstagefright_omx \
     libstagefright_foundation \
