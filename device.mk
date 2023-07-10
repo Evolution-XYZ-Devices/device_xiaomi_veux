19cae36fa0a3965deebf28779e18d4748bc22238
Nippon <adamayyad1999@hotmail.com>
2023-07-10 08:40:39 +0300

    veux: deice.mk: Build android.frameworks.sensorservice@1.0
    Signed-off-by: Nippon <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -117,6 +117,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
