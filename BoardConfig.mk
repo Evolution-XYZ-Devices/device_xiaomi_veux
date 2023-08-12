c4f1dad6fea2ab4d661ba561473f3ba737c6ceee
Kuba Wojciechowski <nullbytepl@gmail.com>
2023-08-12 13:07:10 +0300

    veux: Disable kpti * As per qualcomm - "SM8150/SM8250/SM8350/SM7250/SM7150/SM6150 - KPTI Not required". * It can also help increase performance by a lot in some scenarios.
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,7 @@ BOARD_KERNEL_CMDLINE += swiotlb=0
 BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 
 # Prebuilt Kernel
