bd59553f16771b98d340fa7ce521e3a6fbb53245
Nippon <adamayyad1999@hotmail.com>
2023-08-15 08:25:00 +0300

    Revert "veux: device.mk: Guard qti thermal 2.0 service hals"
    This reverts commit 7f811dcaa365a2ae239276ac89a3e8198ca28329.
    
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -525,10 +525,8 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 
 # Thermal
-ifeq ($(TARGET_USE_QTI_THERMAL_SERVICE),true)
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
-endif
 
 # Update Engine
 PRODUCT_PACKAGES += \
