e49971370e0baccd4e5c982f31e221dccfff0c08
Nippon <adamayyad1999@hotmail.com>
2023-08-15 10:31:11 +0300

    veux: device.mk: Build common Xiaomi 1.0 sensors implementation
    Signed-off-by: Nippon <adamayyad1999@hotmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -112,8 +112,6 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.sensorservice@1.0 \
-    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
@@ -495,6 +493,10 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@1.0-impl-xiaomi \
+    android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.1-service.xiaomi_holi-multihal \
     libsensorndkbridge
 
