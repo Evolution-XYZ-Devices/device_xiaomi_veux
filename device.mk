cd38495987b613df9662413059a3076413c4cdb4
Nippon <adamayyad1999@hotmail.com>
2023-08-16 14:33:44 +0300

    Revert "veux: device.mk: Build common Xiaomi 1.0 sensors implementation"
    This reverts commit 63463c8c98d4b745456df903af5a03c631ec5dc7.
    
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -495,8 +495,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@1.0-impl-xiaomi \
-    android.hardware.sensors@1.0-service \
     android.hardware.sensors@2.1-service.xiaomi_holi-multihal \
     libsensorndkbridge
 
