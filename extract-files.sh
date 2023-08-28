c71c3639bdf1fed910233feea48be5c062dbd6d6
Pranav Vashi <neobuddy89@gmail.com>
2023-08-28 22:58:48 +0800

    veux: Add Moto Dolby support
    This is a squash commit of
    
    sm8150-common: Add dolby to cloned apps exemption list
    Change-Id: I47ab06f7d5152c4c993359ac85d121e41c8df235
    
    sm8150-common: Update dolby mod from v3.0 module
    
    sm8150-common: Link dolby blobs with v33 libstagefright_foundation
    
    sm8150-common: Update daxService from A14
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- extract-files.sh
+++ extract-files.sh
@@ -55,6 +55,14 @@ function blob_fixup() {
             llvm-strip --strip-debug  "${2}"
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
+vendor/etc/seccomp_policy/vendor.qti.hardware.dsp.policy)
+            echo 'madvise: 1' >> ${2}
+            ;;
+        vendor/lib/libstagefright_soft_ddpdec.so | vendor/lib/libstagefright_soft_ac4dec.so | \
+        vendor/lib/libstagefrightdolby.so | vendor/lib64/libstagefright_soft_ddpdec.so | \
+        vendor/lib64/libdlbdsservice.so | vendor/lib64/libstagefright_soft_ac4dec.so | vendor/lib64/libstagefrightdolby.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
     esac
 }
 
