754e633fd3b3aba51eadc255389446713126de8a
Pranav Vashi <neobuddy89@gmail.com>
2023-08-31 14:54:09 +0530

    veux: Enable blur Change-Id: Ib2d7c7b893d0f12e2c52f47ae68f7ab28dda63e2 Signed-off-by: Pranav Vashi <neobuddy89@gmail.com> Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -97,6 +97,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
 
+# Blur
+TARGET_ENABLE_BLUR := true
+
 # Boot Animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
