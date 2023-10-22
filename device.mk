d789703bb9011967e625af3cc051a5016b3f38a9
Sarthak Roy <sarthakroy2002@gmail.com>
2023-10-22 20:18:58 +0300

    veux: Switch to Wifi service AIDL
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -568,7 +568,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi-service \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     hostapd \
