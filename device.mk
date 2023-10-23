2c50be8752eaf9d4292b18fc6da08cd00e7afd4e
JaswalAshish <ashish@m.ms.evolution-x.org>
2023-10-23 12:27:14 +0530

    veux: Build GCamGOPrebuilt-V4
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- device.mk
+++ device.mk
@@ -113,6 +113,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V4
+
+PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
