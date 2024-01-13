b4419264a80f7acf98699913c4ce39d884399484
JaswalAshish <ashish@m.ms.evolution-x.org>
2024-01-13 22:51:21 +0530

    veux: Include ViPER4AndroidFX From - https://github.com/TogoFire/packages_apps_ViPER4AndroidFX
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,15 +1,18 @@
 echo 'Starting to clone stuffs needed for your device'
 
-echo 'Cloning Vendor tree [1/3]'
+echo 'Cloning Vendor tree [1/4]'
 # Vendor Tree
 git clone https://gitlab.com/JaswalAshish/vendor_xiaomi_veux vendor/xiaomi/veux
 
-echo 'Cloning Firmware tree [2/3]'
+echo 'Cloning Firmware tree [2/4]'
 # Firmware Tree
 git clone https://gitlab.com/JaswalAshish/vendor_xiaomi_veux-firmware.git vendor/xiaomi/veux-firmware
 
-echo 'Cloning Kernel tree [3/3]'
+echo 'Cloning Kernel tree [3/4]'
 # Kernel Tree
 git clone --depth=1 https://github.com/dereference23/kernel_xiaomi_sm6375 kernel/xiaomi/sm6375
+
+echo 'Cloning ViPER4AndroidFX [4/4]'
+git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX packages/apps/ViPER4AndroidFX
 
 echo 'Completed, proceeding to lunch'
