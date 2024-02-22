2ea0cc82c84345278dc64a8eb92cb8e6d7158476
mathieuchartier <mathieu.a.chartier@gmail.com>
2024-02-22 17:44:23 +0000

    veux: Speed profile services
    Signed-off-by: Arijit78 <sahaarijit2007@gmail.com>
    Signed-off-by: Prateek Bhatia <bhatiaprateek111@gmail.com>

--- device.mk
+++ device.mk
@@ -538,6 +538,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     frameworks/native/data/etc/android.hardware.sensor.dynamic.head_tracker.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.dynamic.head_tracker.xml
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
